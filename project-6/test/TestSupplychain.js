7c97dc8d5ba97b665adaf2d22d37673613553bf5
Pitipon Pluemworasawat <pitipon@gmail.com>
2022-08-21 00:06:34 +0800

    4th test is passed

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -70,7 +70,7 @@ contract('SupplyChain', function(accounts) {
         assert.equal(resultBufferOne[5], originFarmInformation, 'Error: Missing or Invalid originFarmInformation')
         assert.equal(resultBufferOne[6], originFarmLatitude, 'Error: Missing or Invalid originFarmLatitude')
         assert.equal(resultBufferOne[7], originFarmLongitude, 'Error: Missing or Invalid originFarmLongitude')
-        assert.equal(resultBufferTwo[5], 0, 'Error: Invalid item State')
+        assert.equal(resultBufferTwo[5], 0, 'Error: Invalid item State') // Item state 0 as Harvested
         assert.equal(eventEmitted, true, 'Invalid event emitted')        
     })    
 
@@ -103,7 +103,7 @@ contract('SupplyChain', function(accounts) {
         assert.equal(resultBufferOne[5], originFarmInformation, 'Error: Missing or Invalid originFarmInformation')
         assert.equal(resultBufferOne[6], originFarmLatitude, 'Error: Missing or Invalid originFarmLatitude')
         assert.equal(resultBufferOne[7], originFarmLongitude, 'Error: Missing or Invalid originFarmLongitude')
-        assert.equal(resultBufferTwo[5], 1, 'Error: Invalid item State') // Item state 1 as processed
+        assert.equal(resultBufferTwo[5], 1, 'Error: Invalid item State') // Item state 1 as Processed
         assert.equal(eventEmitted, true, 'Invalid event emitted')  
         
     })    
@@ -137,7 +137,7 @@ contract('SupplyChain', function(accounts) {
         assert.equal(resultBufferOne[5], originFarmInformation, 'Error: Missing or Invalid originFarmInformation')
         assert.equal(resultBufferOne[6], originFarmLatitude, 'Error: Missing or Invalid originFarmLatitude')
         assert.equal(resultBufferOne[7], originFarmLongitude, 'Error: Missing or Invalid originFarmLongitude')
-        assert.equal(resultBufferTwo[5], 2, 'Error: Invalid item State') // Item state 2 as packed
+        assert.equal(resultBufferTwo[5], 2, 'Error: Invalid item State') // Item state 2 as Packed
         assert.equal(eventEmitted, true, 'Invalid event emitted')  
     })    
 
@@ -146,19 +146,32 @@ contract('SupplyChain', function(accounts) {
         const supplyChain = await SupplyChain.deployed()
         
         // Declare and Initialize a variable for event
-        
+        let eventEmitted = false;
         
         // Watch the emitted event ForSale()
-        
+        let event = supplyChain.contract.events;
+        await event.ForSale((err, res) => {
+            eventEmitted = true
+        });
 
         // Mark an item as ForSale by calling function sellItem()
-        
+        await supplyChain.sellItem(upc, productPrice, {from: originFarmerID});
 
         // Retrieve the just now saved item from blockchain by calling function fetchItem()
-        
+        const resultBufferOne = await supplyChain.fetchItemBufferOne.call(upc);
+        const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc);
 
         // Verify the result set
-          
+        assert.equal(resultBufferOne[0], sku, 'Error: Invalid item SKU');
+        assert.equal(resultBufferOne[1], upc, 'Error: Invalid item UPC');
+        assert.equal(resultBufferOne[2], originFarmerID, 'Error: Missing or Invalid ownerID');
+        assert.equal(resultBufferOne[3], originFarmerID, 'Error: Missing or Invalid originFarmerID');
+        assert.equal(resultBufferOne[4], originFarmName, 'Error: Missing or Invalid originFarmName');
+        assert.equal(resultBufferOne[5], originFarmInformation, 'Error: Missing or Invalid originFarmInformation');
+        assert.equal(resultBufferOne[6], originFarmLatitude, 'Error: Missing or Invalid originFarmLatitude');
+        assert.equal(resultBufferOne[7], originFarmLongitude, 'Error: Missing or Invalid originFarmLongitude');
+        assert.equal(resultBufferTwo[5], 3, 'Error: Invalid item State'); // Item state 3 as ForSale
+        assert.equal(eventEmitted, true, 'Invalid event emitted');
     })    
 
     // 5th Test
