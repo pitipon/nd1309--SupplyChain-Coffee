f2d1e0139c08666330430426f23025ece0c3ad24
Pitipon Pluemworasawat <pitipon@gmail.com>
2022-08-20 23:40:39 +0800

    Test: fix bug for running init

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -14,7 +14,7 @@ contract('SupplyChain', function(accounts) {
     const originFarmLongitude = "144.341490"
     var productID = sku + upc
     const productNotes = "Best beans for Espresso"
-    const productPrice = web3.toWei(1, "ether")
+    const productPrice = web3.utils.toWei("1", "ether")
     var itemState = 0
     const distributorID = accounts[2]
     const retailerID = accounts[3]
@@ -49,8 +49,8 @@ contract('SupplyChain', function(accounts) {
         var eventEmitted = false
         
         // Watch the emitted event Harvested()
-        var event = supplyChain.Harvested()
-        await event.watch((err, res) => {
+        var event = supplyChain.contract.events;
+        await event.Harvested((err, res) => {
             eventEmitted = true
         })
 
